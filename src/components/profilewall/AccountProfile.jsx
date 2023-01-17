e6927989d42fa4b173f4dc9ffd1caaa939df8eff
NikZa <nikitos86902002@gmail.com>
2023-01-17 19:25:14 +0300

    AccountProfile update

--- src/components/profilewall/AccountProfile.jsx
+++ src/components/profilewall/AccountProfile.jsx
@@ -2,31 +2,14 @@ import React from "react";
 import "./profile.scss";
 import ProfileContainer from "../profilewall/ProfileContainer";
 import ContentContainer from "../profilewall/ContentContainer";
-import AccountProfileService from "../../api/main/AccountProfileService"
 
 class AccountProfile extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: this.props.match.params.username,
-            value: ''
-        }
-    }
-
-    componentDidMount() {
-        AccountProfileService.retrieveInfo(this.state.username)
-            .then(response => {
-                this.setState({
-                    value: response.data.username
-                });
-            })
-    }
-
     render() {
+        const username = this.props.match.params.username;
         return (
-            <div className="profilePage">
-                <ProfileContainer username={this.state.username}/>
-                <ContentContainer username={this.state.username}/>
+            <div className="profilePage" key={username}>
+                <ProfileContainer username={username}/>
+                <ContentContainer username={username}/>
             </div>
         );
     }
